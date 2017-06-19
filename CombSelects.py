9f15f3f27935475920e97201efc1c8e1abe69301
mjfrancki <mikejfrancki@gmail.com>
2017-06-19 01:23:45 -0400

    Problem 53 solved

---
+++ CombSelects.py
@@ -0,0 +1,39 @@
+'''
+Combinatoric selections
+Problem 53
+
+There are exactly ten ways of selecting three from five, 12345:
+
+123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+In combinatorics, we use the notation, 5C3 = 10.
+
+In general,
+
+nCr  = n! / r!(n-r)!
+where r <= n,
+n! = nx(n-1)x...x3x2x1,
+and 0! = 1.
+It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+
+How many, not necessarily distinct, values of  nCr, for 1 <= n <= 100, are greater than one-million?
+Answer:
+4075
+'''
+
+
+import operator as op
+def ncr(n, r):
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = reduce(op.mul, xrange(n, n-r, -1))
+    denom = reduce(op.mul, xrange(1, r+1))
+    return numer//denom
+
+count = 0
+for n in range(1,101):
+    for r in range(1,n):
+        if ncr(n,r) > 1000000:
+            count += 1
+
+print(count)
