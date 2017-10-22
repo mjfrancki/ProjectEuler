81c14bbc4d23b3708944c9a40f9a7c9f2ad8df31
mjfrancki <mikejfrancki@gmail.com>
2017-10-22 16:07:48 -0400

    Counting summations

---
+++ 076.py
@@ -0,0 +1,30 @@
+"""
+Project Euler Problem 76
+========================
+
+It is possible to write five as a sum in exactly six different ways:
+
+4 + 1
+3 + 2
+3 + 1 + 1
+2 + 2 + 1
+2 + 1 + 1 + 1
+1 + 1 + 1 + 1 + 1
+
+How many different ways can one hundred be written as a sum of at least
+two positive integers?
+"""
+
+
+
+target = 100;
+
+ways = [0] * (target + 1);
+ways[0] = 1;
+
+for i in range(1,100):
+    for j in range(i,target+1):
+        ways[j] += ways[j - i];
+
+
+print ways[target]
